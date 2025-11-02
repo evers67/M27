9f4b6fd1a4a7ca7f6eed86f0e06ce1374bc44cad
evers67 <atarpash@gmail.com>
2025-11-02 21:08:38 +0200

    Update api.jsx

--- front/src/api.jsx
+++ front/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5000"; 
+export const API_BASE_URL = "https://m27-1.onrender.com"; 
 
 const api = axios.create({
   baseURL: API_BASE_URL,
